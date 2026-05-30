b957a12c1b557b9732283ec684b7ea5a9b654a52
Harsh Nadar <harshnadar8979@gmail.com>
2026-05-31 00:11:10 +0530

    Add StudyNotion project files

---
+++ router-project-studyNotation/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import Login from "./Components/Login";
+import Home from "./Components/Home.jsx";
+import Signup from "./Components/Signup";
+import Dashboard from "./Components/Dashboard";
+import PrivateRoute from "./Components/PrivateRoute";
+import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar"
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return <div>
+    <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+      <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn} />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute isLoggedIn={isLoggedIn}>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  </div>
+}
+
+export default App;
